273be0e23af1b45afdcb771858dfd7915211f9ff
Chriveloper <111074541+PuderAlphaBaer@users.noreply.github.com>
2024-12-26 17:10:57 +0100

    Add utility functions and integrate Tailwind CSS; update components and styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://mathlet.chrixel.com',
+  site: 'https://mathlet.chrixel.com',
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
